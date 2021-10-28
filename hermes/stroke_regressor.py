9e22a0c541fcfd4f75218fe19ee79706ac94b3cf
Худяков Юрий <catchitoo@mail.ru>
2021-10-29 02:19:54 +0500

    Fix test_2 and implement read_data

--- hermes/stroke_regressor.py
+++ hermes/stroke_regressor.py
@@ -2,6 +2,4 @@ import pandas as pd
 
 
 def read_data(file):
-    if file == '/tmp/simple.csv':
-        return pd.DataFrame({'Name': ['halp me', 'please'], 'Number': [7, 69], 'Double': [22.8, 13.37]})
-
+    return pd.read_csv(file)
--- test/test_data_reading.py
+++ test/test_data_reading.py
@@ -7,7 +7,7 @@ import numpy as np
 def test_read_data():
     file_loc = '/tmp/simple.csv'
     with open(file_loc, 'w') as f:
-        f.write('Name, Number, Double\nhalp me, 7, 22.8\nplease, 69, 13.37\n')
+        f.write('Name,Number,Double\nhalp me,7,22.8\nplease,69,13.37\n')
     simple = hermes.stroke_regressor.read_data(file_loc)
     assert simple.shape == (2, 3)
     assert simple.equals(pd.DataFrame({'Name': ['halp me', 'please'], 'Number': [7, 69], 'Double': [22.8, 13.37]}))
@@ -19,8 +19,8 @@ def test_read_data_2():
         file_loc = f'/tmp/test{name[i]}.csv'
         data = np.random.uniform(0, 1000, (100, 3))
         df = pd.DataFrame(data=data, columns=['Name', 'Number', 'Double'])
-        df.to_csv(file_loc)
+        df.to_csv(file_loc, index=False)
         simple = hermes.stroke_regressor.read_data(file_loc)
         assert simple.shape == (100, 3)
-        assert simple.equals(df)
+        assert np.allclose(df.to_numpy(), simple.to_numpy())
         
