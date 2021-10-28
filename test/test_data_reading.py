b71c3f6052536cef14a9e72750c889050e9ce809
Худяков Юрий <catchitoo@mail.ru>
2021-10-29 01:04:01 +0500

    Add first test and requirements.txt

---
+++ test/test_data_reading.py
@@ -0,0 +1,11 @@
+import pytest
+import hermes
+import pandas as pd
+
+def test_read_data():
+    file_loc = '/tmp/simple.csv'
+    with open(file_loc, 'w') as f:
+        f.write('Name, Number, Double\nhalp me, 7, 22.8\nplease, 69, 13.37\n')
+    simple = hermes.read_data(file_loc)
+    assert simple.shape == (2, 3)
+    assert simple.equals(pd.DataFrame({'Name': ['halp me', 'please'], 'Number': [7, 69], 'Double': [22.8, 13.37]}))
